04acbc614ea518a603e20c7a4c0aa640db873b8a
Rez <reasongurung702@gmail.com>
2026-01-06 14:13:30 +0545

    first lesson of js

---
+++ JS implementation/script.js
@@ -0,0 +1,12 @@
+// external js
+// alert("hello from external js")
+
+var $number=13;
+var first_name="rez";
+
+
+// variable name must start with a letter or _ or $
+// var cannot contain a whitespace 
+// var cannot be named after keywords
+// var are case sensitive
+
