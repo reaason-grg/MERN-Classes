c353b23faa5823706c2a2e3645755dad957ca083
Rez <reasongurung702@gmail.com>
2026-01-08 14:22:33 +0545

    JS third class

---
+++ Conditional Statement/script.js
@@ -0,0 +1,85 @@
+// let age=2;
+// if (age > 20){
+//     console.log('You can vote.');
+// }else{
+//     console.log("You cannot vote");
+// }
+
+
+
+let n=19;
+if(n%2==0){
+    console.log("Even number");
+}else{
+    console.log("Odd number")
+}
+
+
+
+let marks=50;
+if(marks>90 && marks<=100){
+    console.log("A");
+}else if(marks>80 && marks<=90){
+    console.log("B");
+}else if(marks>70 && marks<=80){
+    console.log("B");
+}else{
+    console.log("Fail")
+}
+
+let num=-20;
+if(num%2==0 && num>0){
+    console.log("Positive Even number");
+}else if(num%2==0 && num<0){
+    console.log("Negative Even number");
+}else if(num%2!=0 && num>0){
+    console.log("Positive Odd number");
+}else{
+    console.log("Negative Odd number")
+}
+
+
+
+// Switch Case
+
+let day=7;
+switch(day){
+    case 1:
+        console.log("Sunday");
+    break;
+    case 2:
+        console.log("Monday");
+    break;
+    case 3:
+        console.log("Tuesday");
+    break;
+    case 4:
+        console.log("Wednesday");
+    break;
+    case 5:
+        console.log("Thrusday");
+    break;
+    case 6:
+        console.log("Friday");
+    break;   
+    case 7:
+        console.log("Saturday");
+    break;
+    default:
+        console.log("Invalid");
+    break;
+}
+
+
+// Early return pattern
+
+let val=100;
+if (val<70){
+    console.log("Failed");
+}else if(val<80){
+    console.log("C");
+}else if(val<90){
+    console.log("B");
+}else{
+    console.log("A");
+}
---
+++ Operators/script.js
@@ -0,0 +1,42 @@
+// Assignment operator
+let num1=34;
+// let num2='34';
+
+// num1+=5;
+// num1=num1+5;
+
+// ------------------------------------------------------------------------------//
+// Comparasion operator always return boolean valye ie.true or false
+
+// == and != checks only value
+// ie. num1==num2 / num1!=num2
+// === and !== checks both value and datatype
+// num1===num2 / num1!==num2
+
+// --------------------------------------------------------------------------------//
+
+// Logical Operator
+
+num1>30 && num1%2==0;
+// && for AND operator
+
+(num1%2==0) || (num1%5==0)
+// || for OR operator
+
+// ! for NOT operator
+
+// ----------------------------------------------------------------------------------//
+// Unary Operator
+num1++;
+// post increment
+++num1;
+// pre increment
+num--;
+// post decrement
+--num1;
+// pre decrement
+
+// ----------------------------------------------------------------------------------//
+// Ternary operator
+let num2= 50 ;
+let result= (num2>100)?'yes':'no';
